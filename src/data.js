87b1677a35d079662141b12461f4180c03f01198
bazzomir <bazzomir97@gmail.com>
2025-04-07 21:03:46 +0200

    added data

---
+++ src/data.js
@@ -0,0 +1,31 @@
+import componentsImg from './assets/image/components.png';
+import propsImg from './assets/image/config.png';
+import jsxImg from './assets/image/jsx-ui.png';
+import stateImg from './assets/image/state-mgmt.png';
+
+export const CORE_CONCEPTS = [
+    {
+        image: componentsImg,
+        title: 'Components',
+        description:
+            'The core UI building block - compose the user interface by combining multiple components.',
+    },
+    {
+        image: jsxImg,
+        title: 'JSX',
+        description:
+            'Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.',
+    },
+    {
+        image: propsImg,
+        title: 'Props',
+        description:
+            'Make components configurable (and therefore reusable) by passing input data to them.',
+    },
+    {
+        image: stateImg,
+        title: 'State',
+        description:
+            'React-managed data which, when changed, causes the component to re-render & the UI to update.',
+    },
+];
