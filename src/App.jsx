454f248623e9e75a2f7ba172a3bd2b85596aa1c6
bazzomir <bazzomir97@gmail.com>
2025-04-12 22:38:05 +0200

    lesson 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { CORE_CONCEPTS } from './data';
+import { useState } from 'react'
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept/CoreConcept';
 import TabButton from './components/TabButton';
 
 
+
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleSelect(selectedButton) {
-    console.log(selectedButton);
+    // tabContent = selectedButton;
+    setSelectedTopic(selectedButton);
   }
 
   return (
@@ -36,7 +40,15 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          Dynamic Content
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
